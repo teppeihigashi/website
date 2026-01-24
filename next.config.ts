b65c782c050c96960852c8938f91fff48c1799ee
teppeihigashi <teppei.higashi@higashigiken.com>
2026-01-24 11:32:15 +0900

    プロフィール写真の追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'higashigiken-pfp.s3.ap-northeast-1.amazonaws.com',
+        pathname: '/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
